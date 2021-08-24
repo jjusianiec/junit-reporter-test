8b45ad3d59cb88678ec3e8e163845113e98b25b5
Jacek Jusianiec <jacek.jusianiec@allegro.pl>
2021-08-24 10:01:05 +0200

    Add junit-reporter

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test1() {
+	}
+
+	@Test
+	void test2() {
+	}
+
+	@Test
+	void test3() {
 	}
 
 }
